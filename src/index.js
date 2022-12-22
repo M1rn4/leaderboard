7f6a766f702903d418ca38f88abd46878baa23ab
mirnamp <ampueromirna8@gmail.com>
2022-12-22 11:36:56 -0500

    modify in html and ndex

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 import './index.css';
+let listOfScore = [];
+const container = document.querySelector('.square');
+const refreshBtn = document.querySelector('.refreshButton');
+const submitBtn = document.querySelector('.btn-submit-score');
+class createObjectScore {
+  constructor(user, score) {
+    this.user =  user;
+    this.score = score;
+  }
+}
+
+class features {
+  static displayListScore(array) {
+    container.innerHTML = '';
+    let backgroundColor = '';
+    for (let i = 0; i < array.length; i += 1) {
+      if (i % 2 === 0) {
+        backgroundColor = 'white';
+      } else {
+        backgroundColor = 'gray';
+      }
+      const scoreTemplate = `
+        <div class="book ${backgroundColor}">
+            <p>"${listOfScore[i].user}"&ensp:</p>
+            <p>&ensp;${listOfScore[i].score}</p>
+        </div>
+            `;
+      container.innerHTML += scoreTemplate;
+    }
+  }
+  static add(user, score) {
+    if (user === '' || score === '') return;
+    const newScore = new createObjectScore(user, score);
+    listOfScore.push(newScore);
+    this.displayListBooks(listOfScore);
+  };
+}
+function addSubmitBtn() {
+  features.add(user.value, score.value);
+  console.log(listOfScore);
+}
+submitBtn.addEventListener('click', addSubmitBtn);
+
+function addRefreshBtn() {
+  features.displayListScore();
+}
+refreshBtn.addEventListener('click', addRefreshBtn);
