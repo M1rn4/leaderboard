3f174e7cca1953b9d09d02000ddd20eb5d5a7042
mirnamp <ampueromirna8@gmail.com>
2022-12-23 10:29:51 -0500

    modify css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import './index.css';
 import getData from './modules/getData.js';
 import newLead from './modules/newLead.js';
+import newId from './modules/newId';
 
 const refreshBtn = document.querySelector('.refreshButton');
 const submitBtn = document.querySelector('.btn-submit-score');
 const userInput = document.getElementById('user');
 const scoreInput = document.getElementById('score');
+const newGameBtn = document.querySelector('.newGame');
 
 refreshBtn.addEventListener('click', () => {
   getData();
@@ -17,4 +19,11 @@ submitBtn.addEventListener('click', async (e) => {
   userInput.value = '';
   scoreInput.value = '';
 });
+newGameBtn.addEventListener('click', async(e) => {
+  e.preventDefault();
+  const generateId = await newId();
+  console.log(generateId);
+  
+})
 getData();
+
